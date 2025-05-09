fa91231dacad439f88201edd87b39efefff6f992
Robert George Leustean <147397130+914RobbLeustean@users.noreply.github.com>
2025-05-09 15:58:35 +0300

    Add files via upload

---
+++ Thrifto/Backend/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+﻿// Controllers/AuthController.cs
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ThriftoServer.DTOs;
+using ThriftoServer.Models;
+using ThriftoServer.Services;
+
+namespace ThriftoServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly TokenService _tokenService;
+
+        public AuthController(UserManager<User> userManager, TokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _userManager.FindByEmailAsync(registerDto.Email) != null)
+            {
+                return BadRequest("Email is already taken");
+            }
+
+            if (await _userManager.FindByNameAsync(registerDto.Username) != null)
+            {
+                return BadRequest("Username is already taken");
+            }
+
+            var user = new User
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.Username,
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return new UserDto
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized("Invalid email or password");
+
+            if (!user.IsActive) return Unauthorized("Account is deactivated");
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!result) return Unauthorized("Invalid email or password");
+
+            return new UserDto
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+    }
+}
